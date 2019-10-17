33a6f850856426f6e1cdf3173798bd48855a46d3
captainchen <1101145957@qq.com>
2019-10-17 20:59:43 +0800

    media search

---
+++ 006-html进阶/js/rem.js
@@ -0,0 +1,22 @@
+/*
+* @Author: Chen
+* @Date:   2019-10-17 18:54:13
+* @Last Modified by:   Chen
+* @Last Modified time: 2019-10-17 19:02:43
+*/
+;(function(win,doc){
+	//根据不同的视口宽度设置不同的根元素字体大小
+	//1.获取根元素
+	var oRoot = doc.getElementsByTagName('html')[0]
+	function refresh(){
+		//2.获取视口宽度
+		var width = doc.body.clientWidth || doc.documentElement.clientWidth;
+		// console.log(width)
+		//3.根据一定比例设置根元素字体大小
+		var oFsize = width / 10;
+		oRoot.style.fontSize = oFsize + 'px';
+	}
+	win.addEventListener('DOMContentLoaded',refresh,false);
+	win.addEventListener('load',refresh,false)
+	win.addEventListener('resize',refresh,false)
+})(window,document)
