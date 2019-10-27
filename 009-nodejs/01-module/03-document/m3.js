b965e122daf22c6e781fee1bd8f65b4e414925f8
captainchen <1101145957@qq.com>
2019-10-27 18:02:44 +0800

    node begin

---
+++ 009-nodejs/01-module/03-document/m3.js
@@ -0,0 +1,9 @@
+/*
+* @Author: Chen
+* @Date:   2019-10-27 17:51:50
+* @Last Modified by:   Chen
+* @Last Modified time: 2019-10-27 17:53:59
+*/
+// console.log(window)
+// console.log(global)
+global.str = 'hello world'
---
+++ 009-nodejs/01-module/03-document/m4.js
@@ -0,0 +1,8 @@
+/*
+* @Author: Chen
+* @Date:   2019-10-27 17:54:19
+* @Last Modified by:   Chen
+* @Last Modified time: 2019-10-27 17:56:05
+*/
+require('./m3.js')
+console.log(global.str)
